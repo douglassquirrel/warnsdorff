aa7cd6ea6875667cd94a473e457c9632e1c9fa45
Douglas Squirrel <ds@douglassquirrel.com>
2009-06-07 06:59:01 +0000

    Clean up

--- tour_to_gif.py
+++ tour_to_gif.py
@@ -6,23 +6,22 @@ import sys
 n = int(sys.argv[1])
 im = Image.new("RGB", (n, n))
 draw = ImageDraw.Draw(im)
-
 draw.rectangle((0, 0, n, n), fill="white")
-im.save("tour0.gif")
 
 files = []
 i=0
 while True:
+    filename = "tour%d.gif" % (i,)
+    im.save(filename)
+    files.append(filename)
+    i+=1
+
     line = sys.stdin.readline().rstrip()
     if not line:
         break
     data = eval(line)
     fill = "red" if data["tiebreak"] else "blue"
     draw.point(data["square"], fill = fill)
-    i+=1
-    filename = "tour%d.gif" % (i,)
-    im.save(filename)
-    files.append(filename)
 
 command = ["convert", "-delay", "1", "-loop", "0"]
 command.extend(files)
