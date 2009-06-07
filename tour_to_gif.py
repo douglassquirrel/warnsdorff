b6e0d3d11ad20836ab7e73a31d0ce7a57298d6bf
Douglas Squirrel <ds@douglassquirrel.com>
2009-06-07 00:00:55 +0000

    Make gifs of tours

--- tour_to_gif.py
+++ tour_to_gif.py
@@ -4,24 +4,27 @@ import subprocess
 import sys
 
 n = int(sys.argv[1])
-print "Generating tour for %d by %d square" % (n, n)
 im = Image.new("RGB", (n, n))
 draw = ImageDraw.Draw(im)
 
-print "Generating frames..."
 draw.rectangle((0, 0, n, n), fill="white")
-im.save("king0.gif")
+im.save("tour0.gif")
 
 files = []
-for i in range(0, n):
-    for j in range(0, n):
-        draw.point((i, j), fill="red")
-        filename = "king%d.gif" % (i*n+j+1,)
-        im.save(filename)
-        files.append(filename)
+i=0
+while True:
+    line = sys.stdin.readline().rstrip()
+    if not line:
+        break
+    data = eval(line)
+    fill = "red" if data["tiebreak"] else "blue"
+    draw.point(data["square"], fill = fill)
+    i+=1
+    filename = "tour%d.gif" % (i,)
+    im.save(filename)
+    files.append(filename)
 
 command = ["convert", "-delay", "1", "-loop", "0"]
 command.extend(files)
-command.append("king.gif")
-print "Calling imagemagick..."
+command.append("tour.gif")
 subprocess.call(command)
